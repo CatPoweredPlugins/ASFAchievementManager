2f0a07aa14b85fc462e7660f2cd3ad5f613dc574
Ryzhehvost <kotlyar.andrey@gmail.com>
2019-10-12 23:30:54 +0300

    update to dotnet 3.0

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 del .\ASFAchievementManager\*.zip
-dotnet publish -c "Release" -f "net472" -o "out/generic-netf"
+dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
 rename .\ASFAchievementManager\ASF-Achievement-Manager.zip ASF-Achievement-Manager-netf.zip
-dotnet publish -c "Release" -f "netcoreapp2.2" -o "out/generic" "/p:LinkDuringPublish=false"
+dotnet publish -c "Release" -f "netcoreapp3.0" -o "out/generic" "/p:LinkDuringPublish=false"
