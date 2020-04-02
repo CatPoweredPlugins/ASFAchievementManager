e7276d17bf2e4f4bc473326540d13464dacccaf4
Ryzhehvost <kotlyar.andrey@gmail.com>
2020-04-02 21:58:43 +0300

    build script update

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+if not exist ArchiSteamFarm\ArchiSteamFarm (git submodule update --init)
+git submodule foreach "git fetch origin; git checkout $(git rev-list --tags --max-count=1);"
 del .\ASFAchievementManager\*.zip
 dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
 rename .\ASFAchievementManager\ASF-Achievement-Manager.zip ASF-Achievement-Manager-netf.zip
-dotnet publish -c "Release" -f "netcoreapp3.0" -o "out/generic" "/p:LinkDuringPublish=false"
+dotnet publish -c "Release" -f "netcoreapp3.1" -o "out/generic" "/p:LinkDuringPublish=false"
--- update.bat
+++
@@ -1,3 +0,0 @@
-:; git submodule foreach 'git fetch origin; git checkout $(git describe --tags `git rev-list --tags --max-count=1`);'; exit $?
-@ECHO OFF
-"C:\Program Files\Git\bin\bash.exe" -c "git submodule foreach 'git fetch origin; git checkout $(git describe --tags `git rev-list --tags --max-count=1`);'"
