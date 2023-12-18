96d2c545a06256f7a0a481175a1915b6f4c1ff7b
Rudokhvist <kotlyar.andrey@gmail.com>
2023-12-18 20:56:53 +0200

    update to net8

--- build.bat
+++ build.bat
@@ -21,7 +21,7 @@ if exist out rmdir /Q /S out
 
 rem release generic version
 
-dotnet publish -c "Release" -f "net6.0" -o "out/generic" "/p:LinkDuringPublish=false"
+dotnet publish -c "Release" -f "net8.0" -o "out/generic" "/p:LinkDuringPublish=false"
 mkdir .\out\%CurrDirName%
 copy .\out\generic\%CurrDirName%.dll .\out\%CurrDirName%
 rem comment section below (downto :zip label) if you don't want to include documentation 
@@ -37,21 +37,3 @@ if ERRORLEVEL 1 (
 7z a -tzip -mx7 .\out\%CurrDirName%.zip .\out\%CurrDirName%
 rmdir /Q /S out\%CurrDirName%
 
-rem release generic-netf version
-rem comment section below if you don't target netf ASF version
-
-dotnet publish -c "Release" -f "net48" -o "out/generic-netf"
-mkdir .\out\%CurrDirName%
-copy .\out\generic-netf\%CurrDirName%.dll .\out\%CurrDirName%
-rem comment section below (downto :zipnetf label) if you don't want to include documentation 
-if not exist README.md (goto zipnetf)
-where /q pandoc.exe
-if ERRORLEVEL 1 (
-  copy README.md .\out\%CurrDirName%
-  goto zipnetf
-) else (
-  pandoc  --metadata title="%CurrDirName%" --standalone --columns 2000 -f markdown -t html --self-contained -c .\github-pandoc.css -o .\out\%CurrDirName%\README.html README.md
-)
-:zipnetf
-7z a -tzip -mx7 .\out\%CurrDirName%-netf.zip .\out\%CurrDirName%
-rmdir /Q /S out\%CurrDirName%
